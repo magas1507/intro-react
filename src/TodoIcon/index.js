40e96ceff26b228a830ca61b668e192622e4e5ab
mgas90 <asmg90@gmail.com>
2021-09-02 18:04:28 -0300

    deploy

--- src/TodoIcon/index.js
+++ src/TodoIcon/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import { ReactComponent as CheckSVG } from './check.svg';
 import { ReactComponent as DeleteSVG } from './delete.svg';
 import './TodoIcon.css';
